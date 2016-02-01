8721ef6752006999c014a86bb13e81402cda0662
Tyler Ruggles <truggles@wisc.edu>
2016-02-01 15:39:17 +0100

    mssmRun now does a combination of all channels (em, tt, mt) which is also plotted in grabAndPlot

--- tester/copy.sh
+++ tester/copy.sh
@@ -1,4 +1,7 @@
-cp /afs/cern.ch/work/t/truggles/Z_to_tautau/ZTTJan14_7_6_3/src/Z_to_TauTau_13TeV/dataCardsShapes/htt/htt_em.inputs-sm-13TeV.root ../../auxiliaries/shapes/Wisconsin/htt_em.inputs-sm-13TeV.root
-cp /afs/cern.ch/work/t/truggles/Z_to_tautau/ZTTJan14_7_6_3/src/Z_to_TauTau_13TeV/dataCardsShapes/htt/htt_tt.inputs-sm-13TeV.root ../../auxiliaries/shapes/Wisconsin/htt_tt.inputs-sm-13TeV.root
-cp /afs/cern.ch/work/t/truggles/Z_to_tautau/ZTTJan14_7_6_3/src/Z_to_TauTau_13TeV/dataCardsShapes/ztt/htt_em.inputs-sm-13TeV.root ../../auxiliaries/shapes/Wisconsin/ztt_em.inputs-sm-13TeV.root
-cp /afs/cern.ch/work/t/truggles/Z_to_tautau/ZTTJan14_7_6_3/src/Z_to_TauTau_13TeV/dataCardsShapes/ztt/htt_tt.inputs-sm-13TeV.root ../../auxiliaries/shapes/Wisconsin/ztt_tt.inputs-sm-13TeV.root
+#cp /afs/cern.ch/work/t/truggles/Z_to_tautau/ZTTJan14_7_6_3/src/Z_to_TauTau_13TeV/dataCardsShapes/htt/htt_em.inputs-sm-13TeV.root ../../auxiliaries/shapes/Wisconsin/htt_em.inputs-sm-13TeV.root
+#cp /afs/cern.ch/work/t/truggles/Z_to_tautau/ZTTJan14_7_6_3/src/Z_to_TauTau_13TeV/dataCardsShapes/htt/htt_tt.inputs-sm-13TeV.root ../../auxiliaries/shapes/Wisconsin/htt_tt.inputs-sm-13TeV.root
+#cp /afs/cern.ch/work/t/truggles/Z_to_tautau/ZTTJan14_7_6_3/src/Z_to_TauTau_13TeV/dataCardsShapes/ztt/htt_em.inputs-sm-13TeV.root ../../auxiliaries/shapes/Wisconsin/ztt_em.inputs-sm-13TeV.root
+#cp /afs/cern.ch/work/t/truggles/Z_to_tautau/ZTTJan14_7_6_3/src/Z_to_TauTau_13TeV/dataCardsShapes/ztt/htt_tt.inputs-sm-13TeV.root ../../auxiliaries/shapes/Wisconsin/ztt_tt.inputs-sm-13TeV.root
+cp /afs/cern.ch/work/t/truggles/Z_to_tautau/ZTTJan14_7_6_3/src/Z_to_TauTau_13TeV/dataCardsShapes/mssm/htt_em.inputs-mssm-13TeV.root ../../auxiliaries/shapes/Wisconsin/htt_em.inputs-mssm-13TeV.root
+cp /afs/cern.ch/work/t/truggles/Z_to_tautau/ZTTJan14_7_6_3/src/Z_to_TauTau_13TeV/dataCardsShapes/mssm/htt_tt.inputs-mssm-13TeV.root ../../auxiliaries/shapes/Wisconsin/htt_tt.inputs-mssm-13TeV.root
+#cp /afs/cern.ch/user/l/ldodd/public/forTyler/* ../../auxiliaries/shapes/Wisconsin/
