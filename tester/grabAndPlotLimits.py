3812149e70f614553c69c5eb19863ff188949df3
Tyler Ruggles <truggles@wisc.edu>
2016-02-01 15:39:17 +0100

    mssmRun now does a combination of all channels (em, tt, mt) which is also plotted in grabAndPlot

--- tester/grabAndPlotLimits.py
+++ tester/grabAndPlotLimits.py
@@ -3,17 +3,15 @@ import array
 from ROOT import gPad
 
 
-masses = [80, 90, 100, 110, 120, 130, 140, 160, 180, 600, 900, 1000, 1200, 1500, 2900, 3200]
+masses1 = [80, 90, 100, 110, 120, 130, 140, 160, 180, 600, 900, 1000, 1200, 1500, 2900, 3200]
 ROOT.gROOT.SetBatch(True)
 
-#binArray = array.array( 'd', [0,] )
-#for m in masses :
-#    binArray.append( m )
-#numBins = len( binArray ) - 1
-#
-#h = ROOT.TH1F('h1','Expected Limits: MSSM H/A#arrow#tau#tau', numBins, binArray)
 
-def plotLimits( channel ) :
+def plotLimits( signal, channel ) :
+
+    #if channel == 'em' or channel == 'tt' or channel == 'mt' : masses = masses1
+    masses = masses1
+
     #limits = [[0 for x in range(5)] for x in range( len(masses) )]
     limits = [[0 for x in range( len(masses) )] for x in range(5)]
     #limits = [array.array( 'd', []) for x in range(5)]
@@ -21,7 +19,10 @@ def plotLimits( channel ) :
 
     mCnt = 0
     for mass in masses :
-        f = ROOT.TFile('%s/%i/higgsCombineTest.Asymptotic.mH%i.root' % (channel, mass, mass), 'r')
+        if channel != 'll' :
+            f = ROOT.TFile('%s/%s/%i/higgsCombineTest.Asymptotic.mH%i.root' % (signal, channel, mass, mass), 'r')
+        else :
+            f = ROOT.TFile('%s/higgsCombineLL.Asymptotic.mH%i.root' % (signal,  mass), 'r')
         t = f.Get('limit')
         print "Channel: ",channel,"   Mass: ",mass
         i = 0
@@ -31,14 +32,10 @@ def plotLimits( channel ) :
             limits[i][mCnt] = row.limit
             #limits[i].append( row.limit )
             i += 1
-
-        #print limits[mCnt]
             
         mCnt += 1
-    #print limits
-    #print limits[0]
-    #print limits[1]
-    #print limits[2]
+
+
     n = len(masses)
     neg2 = ROOT.TGraph( len(masses))
     neg1 = ROOT.TGraph( len(masses))
@@ -58,57 +55,40 @@ def plotLimits( channel ) :
         outShade.SetPoint( i, masses[i],limits[4][i] )
         outShade.SetPoint( n+i, masses[n-i-1],limits[0][n-i-1] )
 
-    #c1 = ROOT.TCanvas( 'c1', 'c1', 600, 600 )
-    #c1.cd()
-    #c1.SetGrid()
-    #c1.DrawFrame( masses[0], 0, masses[-1], max( limits[4] ) )
-    #print c1.DrawFrame( masses[0], 0, masses[-1], 20.0 )
+
     outShade.SetFillStyle(1001)
     outShade.SetFillColor(5)
-    #outShade.Draw('f')
     midShade.SetFillStyle(1001)
     midShade.SetFillColor(3)
-    #midShade.Draw('f')
-    ##c1.DrawFrame()
-    #neg1.Draw('l')
-    #neg2.Draw('l same')
-    #med.Draw('l same')
-    #pos1.Draw('l same')
-    #pos2.Draw('l same')
-    ##p1.Update()
-    ##pad = gPad
-    ##pad.SetLogy()
-    #print gPad
-    #px = gPad.GetPrimitive( "hframe" )
-    #print px
-    #c1.SaveAs('/afs/cern.ch/user/t/truggles/www/limits/limitsTest_%s.png' % channel)
     c2 = ROOT.TCanvas( 'c2', 'c2', 600, 600 )
     p1 = ROOT.TPad( 'p1', 'p1', 0, 0, 1, 1)
     p1.Draw()
     p1.cd()
     med.SetLineStyle(2)
-    outShade.GetXaxis().SetTitle('GeV')
-    outShade.GetYaxis().SetTitle('Cross Section X BR (pb^{1})')
+    outShade.GetXaxis().SetTitle('Visible Mass (GeV)')
+    outShade.GetXaxis().SetTitleOffset( outShade.GetXaxis().GetTitleOffset() * 1.3 )
+    outShade.GetYaxis().SetTitle('95% CL limit on #sigma(gg#phi) x BR(#phi#rightarrow #tau#tau) [pb]')
+    outShade.GetYaxis().SetTitleOffset( outShade.GetYaxis().GetTitleOffset() * 1.3 )
     outShade.SetTitle('Expected Limits A/H #rightarrow #tau#tau: Channel %s' % channel)
     
     outShade.Draw('Af')
     midShade.Draw('f')
-    #neg1.Draw('l')
-    #neg2.Draw('l same')
     med.Draw('l')
-    #pos1.Draw('l same')
-    #pos2.Draw('l same')
-    #px.Draw()
     p1.SetLogy()
+    p1.SetLogx()
     
-    c2.SaveAs('/afs/cern.ch/user/t/truggles/www/limits/limitsTest_%s2.png' % channel)
+    c2.SaveAs('/afs/cern.ch/user/t/truggles/www/limits/Limits_%s_%s.png' % (signal, channel) )
 
     print "PNG files saved here: /afs/cern.ch/user/t/truggles/www/limits/"
     print "They are visible online at: http://truggles.web.cern.ch/truggles/limits/"
-    
-    
 
 
-channels = ['em', 'tt']
-for channel in channels :
-    plotLimits( channel )
+
+
+
+
+channels = ['em', 'tt', 'mt', 'll']
+signals = ['ggH', 'bbH']
+for signal in signals :
+    for channel in channels :
+        plotLimits( signal, channel )
--- tester/mssmRun.sh
+++ tester/mssmRun.sh
@@ -10,25 +10,54 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $DIR
 
+rm -r bbH/ ggH/
 
-for channel in em tt;
+for signal in ggH bbH;
 do
-    for mass in 80 90 100 110 120 130 140 160 180 600 900 1000 1200 1500 2900 3200;
+    for channel in mt em tt ll;
     do
-        mkdir -p ${channel}/${mass}
+        for mass in 80 90 100 110 120 130 140 160 180 600 900 1000 1200 1500 2900 3200;
+        do
+            mkdir -p ${signal}/${channel}/${mass}
+        done
     done
 done
 
-for channel in em tt;
+for signal in ggH bbH;
 do
-    UW_mssm_dataCards ${channel}
-    pushd ${channel}
+    for channel in em tt;
+    do
+        UW_mssm_dataCards ${channel} ${signal}
+        pushd ${signal}/${channel}
+        for mass in 80 90 100 110 120 130 140 160 180 600 900 1000 1200 1500 2900 3200;
+        do
+            pushd ${mass}
+            ln -s $DIR/${signal}/mssm_${channel}.input.root $DIR/${signal}/${channel}/${mass}/mssm_${channel}.input.root
+            combine -M Asymptotic -m ${mass} ${channel}_inclusive_mssm_${mass}.txt
+            popd
+        done
+        popd
+    done
+    for channel in mt; #et;
+    do
+        UW_mssm_dataCards ${channel} ${signal}
+        pushd ${signal}/${channel}
+        for mass in 80 90 100 110 120 130 140 160 180 600 900 1000 1200 1500 2900 3200;
+        do
+            pushd ${mass}
+            ln -s $DIR/${signal}/mssm_${channel}.input.root $DIR/${signal}/${channel}/${mass}/mssm_${channel}.input.root
+            combine -M Asymptotic -m ${mass} ${channel}_inclusivemtnotwoprong_mssm_${mass}.txt
+            popd
+        done
+        popd
+    done
+    pushd ${signal}
     for mass in 80 90 100 110 120 130 140 160 180 600 900 1000 1200 1500 2900 3200;
     do
-        pushd ${mass}
-        ln -s $DIR/mssm_${channel}.input.root $DIR/${channel}/${mass}/mssm_${channel}.input.root
-        combine -M Asymptotic -m ${mass} ${channel}_inclusive_mssm_${mass}.txt
-        popd
+        echo " ---- Doing LL combined ${mass} ---- "
+        combineCards.py em/${mass}/em_inclusive_mssm_${mass}.txt tt/${mass}/tt_inclusive_mssm_${mass}.txt mt/${mass}/mt_inclusivemtnotwoprong_mssm_${mass}.txt > ll/${mass}/ll_inclusive_mssm_${mass}.txt
+        combine -M Asymptotic -m ${mass} -n LL ll/${mass}/ll_inclusive_mssm_${mass}.txt
+        echo " ---- Finishing LL combined ${mass} ---- "
     done
     popd
 done
